c9ddbf14178f890866b1303e5d32d699ccad3a97
dhirajsharma <dhirajsharma20042001@gmail.com>
2022-12-24 22:44:07 +0530

    make program

---
+++ demo.js
@@ -0,0 +1,56 @@
+// const arr = [6, 7, 9, 10];
+
+// let start = arr[0],
+//   last = arr[arr.length - 1];
+// for (let i = start; i < last; i++) {
+//   console.log(i);
+// }
+
+// hariiissh;
+// haris;
+
+const str = "hariiissh";
+
+// //1st approach
+// const str_ = new Set(str);
+// console.log(...str_);
+
+//2nd
+// console.log(
+//   str
+//     .split("")
+//     .filter((val, i, arr) => arr.indexOf(val) === i)
+//     .join("")
+// );
+
+//?Sum of array
+// const arr = [1, 2, 3];
+// let s = 0;
+
+// for (let i = 0; i < arr.length; i++) {
+//   s += arr[i];
+// }
+// console.log(s);
+
+//?largest element in an array
+// let arr = [81, 2, 16, 9, 2];
+// let gt = arr[0];
+
+// for (const val of arr) {
+//   if (val > gt) {
+//     gt = val;
+//   }
+// }
+// console.log(gt);
+
+// const arr = [1, 4, 6];
+// const max = Math.max(...arr);
+// const min = Math.min(...arr);
+// const missing = [];
+
+// for (let i = min; i < max; i++) {
+//   if (!arr.includes(i)) {
+//     missing.push(i);
+//   }
+// }
+// console.log(missing);
